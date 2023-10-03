1e1a3f38d5da98b4a8d5e7e013b08f9f92a8301c
Andoni1Barba <Andoni1barba@gmail.com>
2023-10-03 16:09:31 -0400

    debugbar

--- app/Http/Livewire/ShowThread.php
+++ app/Http/Livewire/ShowThread.php
@@ -30,7 +30,10 @@ class ShowThread extends Component
             'replies' => $this->thread
             ->replies()
             ->whereNull('reply_id')
+            ->with('user','replies.user','replies.replies')
             ->get()
+            
+            
         ]);
     }
 }
